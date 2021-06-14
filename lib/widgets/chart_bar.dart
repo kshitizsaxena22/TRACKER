687e858d4d7a2c56fc98971c81ba5e330324514c
kshitizsaxena22 <kshitizsaxena2209@gmail.com>
2021-06-14 15:05:09 +0530

    chart bar

--- lib/widgets/chart_bar.dart
+++ lib/widgets/chart_bar.dart
@@ -12,7 +12,7 @@ class ChartBar extends StatelessWidget {
         Container(
           height: 20,
           child: FittedBox(
-            child: Text('\$${spendingAmount.toStringAsFixed(0)}'),
+            child: Text('Rs${spendingAmount.toStringAsFixed(0)}'),
           ),
         ),
         SizedBox(
